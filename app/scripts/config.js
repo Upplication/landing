08501dbd546f383e4228ffcd26d03f775c30f643
hhkaos <hhkaos@gmail.com>
2014-03-20 17:14:28 +0100

    Adding development enviroment

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,23 +1,33 @@
 window.$UPP = window.$UPP || {};
 
 $UPP.config = {
+  
   "upplication.com":{
     segmentio_token: "pqbi6xsdfe",
     tagmanager_token: "GTM-PPJS3G",
     host: "desarrollo.upplication.priv",
     env: "PRO"
   },
+
   "upptesting.com":{
     segmentio_token: "j44nuye4y6",
     tagmanager_token: "GTM-M8KJZG",
     host: "prepro.upplication.com",
     env: "PRE"
   },
+
+  "landing.upplication.priv":{
+    segmentio_token: "j44nuye4y6",
+    tagmanager_token: "GTM-M8KJZG",
+    host: "desarrollo.upplication.priv",
+    env: "DES"
+  },
   
   "localhost":{
     segmentio_token: "j44nuye4y6",
     tagmanager_token: "GTM-M8KJZG",
     host: "desarrollo.upplication.priv",
-    env: "DES"
+    env: "LOC"
   }
+
 }
