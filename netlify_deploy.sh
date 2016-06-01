6edea1a368d15ef86ac75cf90b6820b32ae26850
Javier <arnaix@gmail.com>
2016-06-01 22:20:24 +0200

    try alternative netlify publishing

---
+++ netlify_deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# halt script on error
+bash set -e
+
+zip -r website.zip dist
+
+if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
+then
+    curl -H "Content-Type: application/zip" \
+        -H "Authorization: Bearer $token" \
+        --data-binary "@website.zip" \
+        https://api.netlify.com/api/v1/sites/landing-upplication/deploys
+    echo "Build successful, publishing in production environment!"
+
+elif ([ $TRAVIS_BRANCH == "develop" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
+then
+    curl -H "Content-Type: application/zip" \
+            -H "Authorization: Bearer $token" \
+            --data-binary "@website.zip" \
+            https://api.netlify.com/api/v1/sites/landing-dev-upplication/deploys
+    echo "Build successful, publishing in develop environment!"
+
+else
+  echo "Build successful, but not publishing!"
+fi
