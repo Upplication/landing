d0c997e45d7d33837979e63f5e4685279352e1ed
Javier Arnáiz <javier.arnaiz@upplication.com>
2016-06-02 10:59:33 +0200

    fixed netlify id deployment

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -5,18 +5,18 @@ set -ev
 
 zip -r website.zip dist
 
-if [ "$TRAVIS_BRANC" = "master" ] ; then
+if [ "$TRAVIS_BRANCH" = "master" ] ; then
     curl -H "Content-Type: application/zip" \
         -H "Authorization: Bearer $token" \
         --data-binary "@website.zip" \
-        https://api.netlify.com/api/v1/sites/landing-upplication/deploys
+        https://api.netlify.com/api/v1/sites/914901f4-03d3-4a52-b3ce-1a499b5f90f5/deploys
     echo "Build successful, publishing in production environment!"
 
 elif [ "$TRAVIS_BRANCH" = "develop" ] ; then
     curl -H "Content-Type: application/zip" \
             -H "Authorization: Bearer $token" \
             --data-binary "@website.zip" \
-            https://api.netlify.com/api/v1/sites/landing-dev-upplication/deploys
+            https://api.netlify.com/api/v1/sites/47f96b73-9e2b-4d08-9935-767b12fdf79b/deploys
     echo "Build successful, publishing in develop environment!"
 
 else
