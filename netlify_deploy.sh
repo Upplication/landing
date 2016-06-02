c0ffc0e70e4d67b7fcf700eca406b9a0251d1a80
Javier Arnáiz <javier.arnaiz@upplication.com>
2016-06-02 10:33:42 +0200

    fixed travis if statement in netlify_deploy.sh

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -1,20 +1,18 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # halt script on error
-bash set -e
+set -ev
 
 zip -r website.zip dist
 
-if [ "$TRAVIS_BRANCH" == "master" ]
-then
+if [ "$TRAVIS_BRANC" = "master" ] ; then
     curl -H "Content-Type: application/zip" \
         -H "Authorization: Bearer $token" \
         --data-binary "@website.zip" \
         https://api.netlify.com/api/v1/sites/landing-upplication/deploys
     echo "Build successful, publishing in production environment!"
 
-elif [ "$TRAVIS_BRANCH" == "develop" ]
-then
+elif [ "$TRAVIS_BRANCH" = "develop" ] ; then
     curl -H "Content-Type: application/zip" \
             -H "Authorization: Bearer $token" \
             --data-binary "@website.zip" \
