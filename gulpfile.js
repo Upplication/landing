7ef9b9f0a27854923b4ed882aa5b016c727c317c
jarnaiz <javier.arnaiz@upplication.com>
2016-09-23 16:59:29 +0200

    fixed @import cleanCss

--- gulpfile.js
+++ gulpfile.js
@@ -288,7 +288,7 @@ gulp.task('rev:scripts', ['scripts'], function() {
 gulp.task('rev:styles', ['styles'], function() {
     if (gutil.env.type === 'production') {
         return gulp.src('./dist/styles/**/*.css')
-            .pipe(cleanCSS())
+            .pipe(cleanCSS({processImport: false}))// not process import because a timeout error:
             .pipe(rev())
             .pipe(revdel())
             .pipe(gulp.dest('./dist/styles'))
